76b5e8f84ed71c8e192c13c31a2ae2ab63666b1d
Rose Maina <rosewairimu27@gmail.com>
2022-10-24 15:49:43 +0300

    Updated Department Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './Components/MainPage';
-
-
-
 
 function App() {
   return (
     <>
       <Router>
-        <MainPage/>
+        
+          <MainPage />
+      
       </Router> 
       
     </>
--- client/src/Pages/Department.js
+++ client/src/Pages/Department.js
@@ -1,12 +1,107 @@
-import React from "react";
+import React from 'react';
 
+export default class DynamicTable extends React.Component {
+  constructor(props) {
+    super(props);
 
-function Department() {
+    this.state = {
+      department: "",
+      departments: []
+    }
+  }
+
+  updateDepartment(event) {
+    this.setState({
+      department: event.target.value
+    });
+  }
+
+  handleClick() {
+    var departments = this.state.departments;
+
+    departments.push(this.state.department);
+
+    this.setState({
+      departments: departments,
+      department: ""
+    });
+  }
+
+  handleItemChanged(i, event) {
+    var departments = this.state.departments;
+    departments[i]  = event.target.value;
+
+    this.setState({
+      departments: departments
+
+    });
+  }
+
+  handleItemDeleted(i) {
+    var departments = this.state.departments;
+
+    departments.splice(i, 1);
+
+    this.setState({
+      departments: departments
+    });
+  }
+
+  renderRows() {
+    var context = this;
+
+    return  this.state.departments.map(function(o, i) {
+              return (
+                <tr key={"item-" + i}>
+                  <td>
+                    <input
+                      type="text"
+                      value={o}
+                      onChange={context.handleItemChanged.bind(context, i)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={context.handleItemDeleted.bind(context, i)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            });
+  }
+
+  render() {
     return (
-        <>
-            <h1>Department</h1>
-        </>
-    )
+      <div>
+        <table className="">
+          <thead>
+            <tr>
+              <th>
+                Department
+              </th>
+              <th>
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderRows()}
+          </tbody>
+        </table>
+        <hr/>
+        <input
+          type="text"
+          value={this.state.department}
+          onChange={this.updateDepartment.bind(this)}
+        />
+        <button
+          onClick={this.handleClick.bind(this)}
+        >
+          Add Department
+        </button>
+      </div>
+    );
+  }
 }
-
-export default Department;
