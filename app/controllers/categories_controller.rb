a7f33d909174f939cad6e675b66875e9d47d6e20
Rose Maina <rosewairimu27@gmail.com>
2022-10-25 16:47:11 +0300

    Updated controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,42 @@
 class CategoriesController < ApplicationController
+
+    def index
+        categories = Category.all
+        render json: categories, status: :ok
+    end
+
+    def show
+        category = Category.find_by(id: params[:id])
+        render json: category, status: :ok
+    end
+
+    def create
+        category = Category.create!(category_params)
+        render json: category, status: :created
+    end
+
+    def update
+        category = Category.find_by(id: params[:id])
+        if category
+            category.update!(category_params)
+            render json: category, status: :created
+        else
+            render json: {error: "category not found"}, status: :not_found
+        end 
+    end
+
+    def destroy
+        category = Category.find_by(id: params[:id])
+        if category
+            category.destroy
+            head :no_content
+        else 
+            render json: {error: "category not found"}, status: :not_found
+        end
+    end
+    private
+    def category_params
+        params.permit(:name)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        if user
+            user.update!(user_params)
+            render json: user, status: :created
+        else
+            render json: {error: "user not found"}, status: :not_found
+        end 
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        if user
+            user.destroy
+            head :no_content
+        else 
+            render json: {error: "user not found"}, status: :not_found
+        end
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:name)
+    end
 end
