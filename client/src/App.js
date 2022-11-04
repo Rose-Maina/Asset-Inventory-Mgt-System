6ebffb29f5cac8e2efbb9ee99131f8bd1c2a5973
jacksonohallo <jacksonohallo@gmail.com>
2022-11-04 06:03:49 +0300

    add login and logout fuctionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import Login from './Components/Login';
+// import Login from './Components/Login';
 import MainPage from './Pages/Admin/MainPage';
 // import StaffMainPage from './Pages/Employee/StaffMainPage';
 // import ProcurementMainPage from './Pages/Procurement/ProcurementMainPage';
+
 // import FinanceMainPage from './Pages/Finance/FinanceMainPage';
 
 function App() {
   return (
     <>
       <Router>
-        <Login />
+        {/* <Login /> */}
           {/* <StaffMainPage /> */}
           {/* <FinanceMainPage /> */}
           {/* <ProcurementMainPage/> */}
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,60 +1,60 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-// import bg1 from "./bg1.jpg";
+// import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+// // import bg1 from "./bg1.jpg";
 
 
 
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+// function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
-  const data = {
-    email: email,
-    password: password,
-  };
+//   const navigate = useNavigate();
+//   const data = {
+//     email: email,
+//     password: password,
+//   };
 
-  function submitLoginForm(e) {
-    e.preventDefault();
+//   function submitLoginForm(e) {
+//     e.preventDefault();
     
-    axios.post("/login", data)
+//     axios.post("/login", data)
     
-    .then(navigate("/dashboard"));
-  }
+//     .then(navigate("/dashboard"));
+//   }
 
-  // style={{ backgroundImage: `url(${bg1})` }}
+//   // style={{ backgroundImage: `url(${bg1})` }}
 
-  return (
-    <div className="container py-5">
-      <center>
-        <div className="py-5">
-          <div style={{ maxWidth: 500 }}>
-            <div className="py-5">
-              <form onSubmit={submitLoginForm}>
-                <div className="py-5">
-                <h3 className="py-4"><strong>Sign In</strong></h3> 
-                </div>
-                  <div className="form-group">
-                    <label><strong>Email</strong></label>
-                    <input type="email" value={email} onChange={e=>setEmail(e.target.value)} className="form-control" required />
-                  </div>
-                  <div className="form-group">
-                    <label><strong>Password</strong></label>
-                    <input  type="password" value={password} onChange={e=>setPassword(e.target.value)} className="form-control" required />
-                  </div>
-                <div className="py-5">
-                  <button type="submit" className="btn btn-success" value="Add">
-                    LOGIN
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </center>
-    </div>
-  );
-}
+//   return (
+//     <div className="container py-5">
+//       <center>
+//         <div className="py-5">
+//           <div style={{ maxWidth: 500 }}>
+//             <div className="py-5">
+//               <form onSubmit={submitLoginForm}>
+//                 <div className="py-5">
+//                 <h3 className="py-4"><strong>Sign In</strong></h3> 
+//                 </div>
+//                   <div className="form-group">
+//                     <label><strong>Email</strong></label>
+//                     <input type="email" value={email} onChange={e=>setEmail(e.target.value)} className="form-control" required />
+//                   </div>
+//                   <div className="form-group">
+//                     <label><strong>Password</strong></label>
+//                     <input  type="password" value={password} onChange={e=>setPassword(e.target.value)} className="form-control" required />
+//                   </div>
+//                 <div className="py-5">
+//                   <button type="submit" className="btn btn-success" value="Add">
+//                     LOGIN
+//                   </button>
+//                 </div>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </center>
+//     </div>
+//   );
+// }
 
-export default Login;
+// export default Login;
